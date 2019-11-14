77cda167f03641c2110c9cfa932e382cfc38d3c4
ancyrajan96 <53744652+ancyrajan96@users.noreply.github.com>
2019-11-14 13:33:39 +0530

    Add files via upload

---
+++ admin_application.php
@@ -0,0 +1,424 @@
+<?php
+include("connection.php");
+session_start();
+ob_start();
+if(isset($_SESSION['admin'])){
+    
+}
+else{
+    header("location:index.php");
+}
+if(isset($_POST['sub'])){
+    $rpl=$_POST['rpl'];
+    $dt=date('Y-m-d');
+    $ins_rpl=mysqli_query($con,"insert into app_rply values('','".$_GET['apid']."','$dt','$rpl')");
+    if($ins_rpl>0){
+        header("location:admin_application.php?apid=".$_GET['apid']);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--
+        ===
+        This comment should NOT be removed.
+
+        Charisma v2.0.0
+
+        Copyright 2012-2014 Muhammad Usman
+        Licensed under the Apache License v2.0
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        http://usman.it
+        http://twitter.com/halalit_usman
+        ===
+    -->
+    <meta charset="utf-8">
+    <title>Admin Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
+    <meta name="author" content="Muhammad Usman">
+
+    <!-- The styles -->
+    <link id="bs-css" href="css/bootstrap-cerulean.min.css" rel="stylesheet">
+
+    <link href="css/charisma-app.css" rel="stylesheet">
+    <link href='bower_components/fullcalendar/dist/fullcalendar.css' rel='stylesheet'>
+    <link href='bower_components/fullcalendar/dist/fullcalendar.print.css' rel='stylesheet' media='print'>
+    <link href='bower_components/chosen/chosen.min.css' rel='stylesheet'>
+    <link href='bower_components/colorbox/example3/colorbox.css' rel='stylesheet'>
+    <link href='bower_components/responsive-tables/responsive-tables.css' rel='stylesheet'>
+    <link href='bower_components/bootstrap-tour/build/css/bootstrap-tour.min.css' rel='stylesheet'>
+    <link href='css/jquery.noty.css' rel='stylesheet'>
+    <link href='css/noty_theme_default.css' rel='stylesheet'>
+    <link href='css/elfinder.min.css' rel='stylesheet'>
+    <link href='css/elfinder.theme.css' rel='stylesheet'>
+    <link href='css/jquery.iphone.toggle.css' rel='stylesheet'>
+    <link href='css/uploadify.css' rel='stylesheet'>
+    <link href='css/animate.min.css' rel='stylesheet'>
+
+    <!-- jQuery -->
+    <script src="bower_components/jquery/jquery.min.js"></script>
+
+    <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- The fav icon -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+</head>
+
+<body>
+    <!-- topbar starts -->
+    <div class="navbar navbar-default" role="navigation">
+
+        <div class="navbar-inner">
+            <button type="button" class="navbar-toggle pull-left animated flip">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" style="width: 250px;" href="adminhome.php"> <img alt="Charisma Logo" src="img/logo20.png" class="hidden-xs"/>
+                <span>Public Utility </span></a>
+
+            <!-- user dropdown starts -->
+            <div class="btn-group pull-right">
+                <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    <i class="glyphicon glyphicon-user"></i><span class="hidden-sm hidden-xs"> admin</span>
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Profile</a></li>
+                    <li class="divider"></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+            <!-- user dropdown ends -->
+
+            <!-- theme selector starts -->
+            <div class="btn-group pull-right theme-container animated tada">
+                <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    <i class="glyphicon glyphicon-tint"></i><span
+                        class="hidden-sm hidden-xs"> Change Theme / Skin</span>
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" id="themes">
+                    <li><a data-value="classic" href="#"><i class="whitespace"></i> Classic</a></li>
+                    <li><a data-value="cerulean" href="#"><i class="whitespace"></i> Cerulean</a></li>
+                    <li><a data-value="cyborg" href="#"><i class="whitespace"></i> Cyborg</a></li>
+                    <li><a data-value="simplex" href="#"><i class="whitespace"></i> Simplex</a></li>
+                    <li><a data-value="darkly" href="#"><i class="whitespace"></i> Darkly</a></li>
+                    <li><a data-value="lumen" href="#"><i class="whitespace"></i> Lumen</a></li>
+                    <li><a data-value="slate" href="#"><i class="whitespace"></i> Slate</a></li>
+                    <li><a data-value="spacelab" href="#"><i class="whitespace"></i> Spacelab</a></li>
+                    <li><a data-value="united" href="#"><i class="whitespace"></i> United</a></li>
+                </ul>
+            </div>
+            <!-- theme selector ends -->
+
+            
+
+        </div>
+    </div>
+    <!-- topbar ends -->
+<div class="ch-container">
+    <div class="row">
+        
+        <!-- left menu starts -->
+        <div class="col-sm-2 col-lg-2">
+            <div class="sidebar-nav">
+                <div class="nav-canvas">
+                    <div class="nav-sm nav nav-stacked">
+
+                    </div>
+                    <ul class="nav nav-pills nav-stacked main-menu">
+                        <li class="nav-header">Main</li>
+                        <li><a class="ajax-link" href="adminhome.php"><i class="glyphicon glyphicon-home"></i><span> Dashboard</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="loc_manage.php"><i class="glyphicon glyphicon-globe "></i><span> Manage Location</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="tal_manage.php"><i class="glyphicon glyphicon-star "></i><span> Manage Taluk</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="vil_manage.php"><i class="glyphicon glyphicon-star-empty "></i><span> Manage Village</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="admin_inbox.php"><i class="glyphicon glyphicon-envelope"></i><span> Inbox</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="p_msg.php"><i class="glyphicon glyphicon-warning-sign"></i><span> Msg to Public</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="admin_cmplnt.php"><i class="glyphicon glyphicon-arrow-down"></i><span> Complaints</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="admin_fdbk.php"><i class="glyphicon glyphicon-folder-open"></i><span> FeedBacks</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="admin_msg.php"><i class="glyphicon glyphicon-file"></i><span> Msg to Village</span></a>
+                        </li>
+                        
+                    </ul>
+                    
+                </div>
+            </div>
+        </div>
+        <!--/span-->
+        <!-- left menu ends -->
+
+        
+
+        <div id="content" class="col-lg-10 col-sm-10">
+            <!-- content starts -->
+            <div>
+    <ul class="breadcrumb">
+        <li>
+            <a href="adminhome.php">Home</a>
+        </li>
+        <li>
+            <a href="adminhome.php">Dashboard</a>
+        </li>
+    </ul>
+</div>
+
+
+<div class="row">
+    <div class="box col-md-12">
+        <div class="box-inner">
+            <div class="box-header well">
+                <h2><i class="glyphicon glyphicon-info-sign"></i> Direct Application</h2>
+
+                <div class="box-icon">
+                    <a href="#" class="btn btn-setting btn-round btn-default"><i
+                            class="glyphicon glyphicon-cog"></i></a>
+                    <a href="#" class="btn btn-minimize btn-round btn-default"><i
+                            class="glyphicon glyphicon-chevron-up"></i></a>
+                    <a href="#" class="btn btn-close btn-round btn-default"><i
+                            class="glyphicon glyphicon-remove"></i></a>
+                </div>
+            </div>
+            <div class="box-content row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="col-lg-6">
+                        <?php
+                        $sel=mysqli_query($con,"select * from sec_app order by id desc");
+                        if(mysqli_num_rows($sel)>0){
+                            ?>
+                        <table class="table table-bordered table-condensed">
+                            <tr>
+                                <td>#</td>
+                                <td>Application Title</td>
+                                <td>Posted By</td>
+                                <td>Read</td>
+                            </tr>
+                        <?php
+                        $i=1;
+                            while($r_app=mysqli_fetch_row($sel))
+                            {
+                                ?>
+                            <tr>
+                                <td><?php echo $i ?></td>
+                                <td><?php echo $r_app[1] ?></td>
+                                <td><?php echo $r_app[5] ?></td>
+                                <td><a href="admin_application.php?apid=<?php echo $r_app[0] ?>"><img src="logo/read.png" /></a></td>
+                            </tr>
+                            <?php
+                            $i++;
+                            }
+                            ?>
+                        </table>
+                            <?php
+                        }
+                        else{
+                            echo"No Application Found";
+                        }
+                        ?>
+                        
+                    </div>
+                    <div class="col-lg-6">
+                       <?php
+                       if(isset($_GET['apid'])){
+                           $up=mysqli_query($con,"update sec_app set st=1 where id='".$_GET['apid']."'");
+                           $sel_app=mysqli_query($con,"select * from sec_app where id='".$_GET['apid']."'");
+                           $rapp=mysqli_fetch_row($sel_app);
+                           ?>
+                        <form method="post">
+                            <script type="text/javascript">
+                            function sho_msg(){
+                                $("#rp").toggle(1000);
+                            }
+                            </script>
+                        <table class="table table-bordered table-condensed">
+                            <tr>
+                                <td colspan="2">Posted By :<b> <?php echo $rapp[5] ?></b></td>
+                            </tr>
+                            <tr>
+                                <td>Application Title</td>
+                                <td><?php echo $rapp[1] ?></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <b>Description</b><br />
+                                    <?php echo $rapp[2] ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>View Attachment</td>
+                                <td><a href="usr_app/<?php echo $rapp[3] ?>" target="_blank">View</a> </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><div onclick="sho_msg()"><center><b>PREVIOUS REPLAYS</b></center></div></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <span id="rp" style="display: none;">
+                                        <?php
+                                        $sel_rp=mysqli_query($con,"select * from app_rply where appid='".$_GET['apid']."'");
+                                        if(mysqli_num_rows($sel_rp)>0){
+                                            ?>
+                                        <table class="table table-bordered table-condensed">
+                                            <tr>
+                                                <td width="100">Date</td>
+                                                <td>Message</td>
+                                            </tr>
+                                        <?php
+                                            while($r_rp=mysqli_fetch_row($sel_rp))
+                                            {
+                                                ?>
+                                            <tr>
+                                                <td><?php echo $r_rp[2] ?></td>
+                                                <td><?php echo $r_rp[3] ?></td>
+                                            </tr>
+                                            <?php
+                                            }
+                                            ?>
+                                        </table>
+                                            <?php
+                                        }
+                                        else{
+                                            echo "No Replay Posted";
+                                        }
+                                        ?>
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><center>POST REPLAY</center></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><textarea name="rpl" class="form-control"></textarea></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><center><input type="submit" name="sub" value="post Reply" class="btn btn-success" /></center></td>
+                            </tr>
+                        </table>
+                            </form>
+                        <?php
+                       }
+                       ?>
+                        
+                    </div>
+                </div>
+                <!-- Ads, you can remove these -->
+                
+
+                
+                <!-- Ads end -->
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    
+    <!--/span-->
+
+    
+    <!--/span-->
+
+    
+    <!--/span-->
+</div><!--/row-->
+
+
+    <!-- content ends -->
+    </div><!--/#content.col-md-0-->
+</div><!--/fluid-row-->
+
+    <!-- Ad, you can remove it -->
+    <div class="row">
+       
+        
+
+    </div>
+    <!-- Ad ends -->
+
+    <hr>
+
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+         aria-hidden="true">
+
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">×</button>
+                    <h3>Settings</h3>
+                </div>
+                <div class="modal-body">
+                    <p>Here settings can be configured...</p>
+                </div>
+                <div class="modal-footer">
+                    <a href="#" class="btn btn-default" data-dismiss="modal">Close</a>
+                    <a href="#" class="btn btn-primary" data-dismiss="modal">Save changes</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="row">
+        <p class="col-md-9 col-sm-9 col-xs-12 copyright">&copy; <a href="#" target="_blank">Trinity Technologies</a> 2015 - 2016</p>
+
+        <p class="col-md-3 col-sm-3 col-xs-12 powered-by">Powered by: <a
+                href="#">Trinity</a></p>
+    </footer>
+
+</div><!--/.fluid-container-->
+
+<!-- external javascript -->
+
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- library for cookie management -->
+<script src="js/jquery.cookie.js"></script>
+<!-- calender plugin -->
+<script src='bower_components/moment/min/moment.min.js'></script>
+<script src='bower_components/fullcalendar/dist/fullcalendar.min.js'></script>
+<!-- data table plugin -->
+<script src='js/jquery.dataTables.min.js'></script>
+
+<!-- select or dropdown enhancer -->
+<script src="bower_components/chosen/chosen.jquery.min.js"></script>
+<!-- plugin for gallery image view -->
+<script src="bower_components/colorbox/jquery.colorbox-min.js"></script>
+<!-- notification plugin -->
+<script src="js/jquery.noty.js"></script>
+<!-- library for making tables responsive -->
+<script src="bower_components/responsive-tables/responsive-tables.js"></script>
+<!-- tour plugin -->
+<script src="bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js"></script>
+<!-- star rating plugin -->
+<script src="js/jquery.raty.min.js"></script>
+<!-- for iOS style toggle switch -->
+<script src="js/jquery.iphone.toggle.js"></script>
+<!-- autogrowing textarea plugin -->
+<script src="js/jquery.autogrow-textarea.js"></script>
+<!-- multiple file upload plugin -->
+<script src="js/jquery.uploadify-3.1.min.js"></script>
+<!-- history.js for cross-browser state change on ajax -->
+<script src="js/jquery.history.js"></script>
+<!-- application script for Charisma demo -->
+<script src="js/charisma.js"></script>
+
+
+</body>
+</html>
